7030cb9472e9363665422e1e625bf2568e2926e6
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-10-10 17:40:54 +0300

    add find word containing character

--- leetCode/2942_find_words_containing_character.go
+++ leetCode/2942_find_words_containing_character.go
@@ -16,6 +16,24 @@ func findWordsContaining(words []string, x byte) []int {
 	return wordContainingX
 }
 
+func quadOrdering(arr []int) []int {
+	result := make([]int, len(arr))
+	i, j, pos := 0, len(arr)-1, len(arr)-1
+
+	for i <= j {
+		if arr[i]*arr[i] > arr[j]*arr[j] {
+			result[pos] = arr[i] * arr[i]
+			i++
+		} else {
+			result[pos] = arr[j] * arr[j]
+			j--
+		}
+		pos--
+	}
+	return result
+}
+
 func main() {
 	fmt.Println(findWordsContaining([]string{"leetcode", "stack", "binary tree"}, 'e'))
+	fmt.Println(quadOrdering([]int{-5, -3, 2, 4}))
 }
