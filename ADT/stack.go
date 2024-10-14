391c7c739bf902ac6389db3baa0e1b68fceac40e
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-10-14 15:31:32 +0300

    add pop method

--- ADT/stack.go
+++ ADT/stack.go
@@ -1,11 +1,23 @@
 package main
 
+import "errors"
+
 type Stack struct {
 	items []int
 }
 
 func (s *Stack) Push(value int) {
 	s.items = append(s.items, value)
+}
+
+func (s *Stack) Pop() (int, error) {
+	if len(s.items) == 0 {
+		return 0, errors.New("stack is empty")
+	}
+
+	poppedItem := s.items[len(s.items)-1]
+	s.items = s.items[:poppedItem]
+	return poppedItem, nil
 }
 
 func main() {}
