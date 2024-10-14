60515aaa3a3001049021ee80ae23f8ee1d17e4be
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-10-14 14:54:40 +0300

    add Insert method to the bt

--- ADT/bt.go
+++ ADT/bt.go
@@ -2,13 +2,37 @@ package main
 
 type Node struct {
 	Value int
-	Next  *Node
+	Left  *Node
+	Right *Node
 }
 
 type BinaryTree struct {
 	root *Node
 }
 
-func main() {
+func (bt *BinaryTree) Insert(value int) {
+	bt.root = InsertNode(bt.root, value)
+}
 
+func InsertNode(node *Node, value int) *Node {
+	if node == nil {
+		return &Node{value, nil, nil}
+	}
+
+	if value < node.Value {
+		node.Left = InsertNode(node.Left, value)
+	}
+	if value > node.Value {
+		node.Right = InsertNode(node.Right, value)
+	}
+	return node
+}
+
+func main() {
+	bt := &BinaryTree{}
+	bt.Insert(100)
+	bt.Insert(50)
+	bt.Insert(120)
+	bt.Insert(30)
+	bt.Insert(200)
 }
