5bcdaf9a77df6b88538b50e440d596f250b37632
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-10-14 15:45:36 +0300

    fix

--- strings/palindrome.go
+++ strings/palindrome.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"unicode"
 )
@@ -30,49 +29,6 @@ func checkPalindrome(word string) bool {
 	//	right--
 	//}
 	//return true
-}
-
-func concatStringsBad(string1, string2 string) (string, error) {
-	var resString string
-
-	if string1 == "" {
-		return "", errors.New("string1 is empty")
-	} else {
-		if string2 == "" {
-			return "", errors.New("string2 is empty")
-		} else {
-			resString = string1 + string2
-		}
-	}
-	return resString, nil
-}
-
-func concatStringsGood(s1, s2 string, maxLength int) (string, error) {
-	if s1 == "" {
-		return "", errors.New("string1 is empty")
-	}
-	if s2 == "" {
-		return "", errors.New("string2 is empty")
-	}
-
-	resString, err := concatString(s1, s2)
-	if err != nil {
-		return "", err
-	}
-
-	if len(resString) > maxLength {
-		return resString[:maxLength], nil
-	}
-
-	return resString, nil
-
-}
-
-func concatString(s1, s2 string) (string, error) {
-	if len(s1)+len(s2) < len(s1) || len(s1)+len(s2) < len(s2) {
-		return "", errors.New("concatenation overflow")
-	}
-	return s1 + s2, nil
 }
 
 func main() {
