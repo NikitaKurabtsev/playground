cd731efa4f9f9a7cecb6da8071b5e7ffa11ceaef
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-10-14 15:10:46 +0300

    add map getkeys with a generics

--- map/playground.go
+++ map/playground.go
@@ -1,1 +1,14 @@
-package _map
+package main
+
+func getKeys[K comparable, V any](m map[K]V) []K {
+	var keys []K
+
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func main() {
+
+}
